9715f32c3ffa379ff9d90722c780f3b9501a4ec3
Sally Alvarenga <sallyalvarenga35@gmail.com>
2021-08-13 12:39:41 -0500

    added my name to everthing and updated html

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-// Need to create show and hide classes for index. html to hide first home page, and show second page.
-// Need to get api data from library of congress and append it to the page. 
-// for the search we can As a user, I can either perform a generic search for data in all formats or I can select a format in the form to help filter results.
+// Need to create show and hide classes for index. html to hide first home page, and show second page. -Sally
+// Need to get api data from library of congress and append it to the page. -SAlly
+// for the search we can As a user, I can either perform a generic search for data in all formats or I can select a format in the form to help filter results. -Sally
+// -Sally
+// -sally
