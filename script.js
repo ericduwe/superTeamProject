a6535084a1c008c247e5e6cfdd8734a43543b033
Jeffrey McLeod <jeffreymmcleod@outlook.com>
2021-08-13 12:47:13 -0500

    dont commit this

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 // Need to create show and hide classes for index. html to hide first home page, and show second page.
+<<<<<<< HEAD
 // Need to get api data from library of congress 
 // aasdfasdfasdf
 //and append it to the page. 
 // for the search we can As a user, I can either perform a generic search for data in all formats or I can select a format in the form to help filter results.
+=======
+// Need to get api data from library of congress and append it to the page. 
+// for the search we can As a user, I can either perform a generic search for data in all
+// formats or I can select a format in the form to help filter results.
+// adsfasdfasdfasdfadsfg
+>>>>>>> 06afd5589c9b5ead536a92f30e03f27dce04d101
