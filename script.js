eb8687025ea86d23944f84c3e29f2133af02bf33
Eric Duwe <ericduwe@gmail.com>
2021-08-13 12:39:27 -0500

    new commit

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
 // Need to create show and hide classes for index. html to hide first home page, and show second page.
 // Need to get api data from library of congress and append it to the page. 
-<<<<<<< HEAD
 // for the search we can As a user, I can either perform a generic search for data in all formats or I can select a format in the form to help filter results.
 
 
 function messup {
    console.log("this is messing everything up!")
 }
-=======
-// for the search we can As a user, I can either perform a generic search for data in all
-// formats or I can select a format in the form to help filter results.
-// adsfasdfasdfasdfadsfg
->>>>>>> 06afd5589c9b5ead536a92f30e03f27dce04d101
